32b50931179cbe55c7436007e448bc3358d323b5
saifulmillah92 <saifulmillah92@gmail.com>
2025-12-29 11:21:44 +0700

    initialize

---
+++ lib/rarchitecture.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rarchitecture/version"
+
+module Rarchitecture
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rarchitecture_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rarchitecture do
+  it "has a version number" do
+    expect(Rarchitecture::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
